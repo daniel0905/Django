9fde19f4e7892520942cbdcca491daeb340967a4
daniel0905 <duongminhtam.it@gmail.com>
2015-11-06 09:45:55 +0700

    Delete project

--- BookList/viewbook/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from viewbook import views
-
-urlpatterns = [
-    url(r'^page(?P<page>[0-9]+)/', views.ViewListBook.as_view(), name="viewlistbook"),
-    url(r'^', views.ViewListBook.as_view(), name="viewlistbook1")
-]
